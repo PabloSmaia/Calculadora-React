0429fe3ae09f59e55ace7e974b22d29c1399d8e4
Pablo Maia <pablosmaia@gmail.com>
2022-09-07 10:19:03 -0300

    Projeto finalizado

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Calculadora from './components/Calculadora'
+import "./components/index.css"
+
+function App() {
+    return (
+        <div>
+            <h1>Calculadora</h1>
+            <Calculadora />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
